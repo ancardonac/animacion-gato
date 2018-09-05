9f10fd80c3a5e40dd32952420726eba81d567753
Estudiante <Estudiante@10.203.2.220>
2018-09-05 17:58:28 +0000

    Java doc

---
+++ javadoc/src/javadoc/Javadoc.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javadoc;
+
+/**
+ * Dewscripcion 
+ * @author Estudiante
+ * @version 1.0
+ * @since 06/09/2018
+ */
+public class Javadoc {
+
+    /**
+     * Explicacion del significado del metodpo
+     * @param args opciones por linea de comandos 
+     */
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    
+    /**
+ * @param yy significado del parametro
+ * @param zz gnificado del parametro
+ * @return explicacion funcion
+ */
+public int xx(float yy, double zz){
+return 0;
+
+}
+}
+
